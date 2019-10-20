ffcb01fd873421e515e19ac19108478df98f6b86
Landon Lemieux <landonlemieux@u.boisestate.edu>
2019-10-20 16:15:53 -0600

    missing header

---
+++ header.php
@@ -0,0 +1,21 @@
+<header class="main-header">
+    <nav class="nav main-nav">
+        <!-- tells the website that there are links here-->
+        <div id="nav-container">
+            <div class="left">
+                <a href="home.html">LOGO IN TRAINING</a>
+            </div>
+
+            <div class="center">
+                <ul>
+                    <li><a href="tickets.html">TICKETS</a></li>
+                    <li><a href="about.html">CONTACT</a></li>
+                </ul>
+            </div>
+
+            <div class="right">
+                <a href="login.html">LOGIN</a>
+            </div>
+        </div>
+    </nav>
+</header>
