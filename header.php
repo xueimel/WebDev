ab4ad27faefeeb044484f64ab51993401e9767fc
Landon Lemieux <landonlemieux@u.boisestate.edu>
2019-10-20 17:11:25 -0600

    asdf

--- header.php
+++ header.php
@@ -3,18 +3,18 @@
         <!-- tells the website that there are links here-->
         <div id="nav-container">
             <div class="left">
-                <a href="home.html">LOGO IN TRAINING</a>
+                <a href="home.php">LOGO IN TRAINING</a>
             </div>
 
             <div class="center">
                 <ul>
-                    <li><a href="tickets.html">TICKETS</a></li>
-                    <li><a href="about.html">CONTACT</a></li>
+                    <li><a href="tickets.php">TICKETS</a></li>
+                    <li><a href="about.php">CONTACT</a></li>
                 </ul>
             </div>
 
             <div class="right">
-                <a href="login.html">LOGIN</a>
+                <a href="login.php">LOGIN</a>
             </div>
         </div>
     </nav>
