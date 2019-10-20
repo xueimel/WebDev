f2dfd023415f6599b98b14c7713793a49d3663cb
Landon Lemieux <landonlemieux@u.boisestate.edu>
2019-10-20 15:53:47 -0600

    updated links for php

--- tickets.php
+++ tickets.php
@@ -11,19 +11,19 @@
             <!-- tells the website that there are links here-->
             <div id="nav-container">
                 <div class="left">
-                    <a href="home.html">LOGO IN TRAINING</a>
+                    <a href="home.php">LOGO IN TRAINING</a>
                 </div>
 
                 <div class="center">
                     <ul>
                         <!-- unordered list-->
-                        <li id="center-right"><a href="tickets.html">TICKETS</a></li>
-                        <li id="center-left"><a href="about.html">CONTACT/part that refuses to work</a></li>
+                        <li id="center-right"><a href="tickets.php">TICKETS</a></li>
+                        <li id="center-left"><a href="about.php">CONTACT/part that refuses to work</a></li>
 
                     </ul>
                 </div>
                 <div class="right">
-                    <a href="login.html">LOGIN</a>
+                    <a href="login.php">LOGIN</a>
                 </div>
             </div>
         </nav>
